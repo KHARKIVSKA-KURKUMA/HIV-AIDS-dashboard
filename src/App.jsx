b81be5d2385de7dc18b994af14169c42f9f0f59d
Kurkuma Laptop <catherine.vasyuk11@gmail.com>
2026-05-10 21:39:22 +0300

    destructuring app

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import AnotherPage from "./components/AnotherPage/AnotherPage";
+import Layout from "./components/Layout/Layout";
+// import { GlobalStyle } from "./GlobalStyles";
+
+function App() {
+  return (
+    <>
+      {/* <GlobalStyle /> */}
+      <BrowserRouter basename={import.meta.env.DEV ? "/" : "/FluChart/"}>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="example" element={<AnotherPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
